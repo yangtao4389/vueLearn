7905ed76f674dea315e65f346bcba8f33c541a53
yangtao <438936731@qq.com>
2019-02-14 16:49:21 +0800

    首页开始

---
+++ vue-project-1/README.md
@@ -0,0 +1,38 @@
+### 首先创建项目
+webpack + vue + vue-router
+npm i
+
+### 分析首页：
+顶部，底部，与中间路由部分
+
+顶部使用mint-ui
+具体参考：https://mint-ui.github.io/docs/#/zh-cn2/quickstart
+
+
+底部参考mui
+直接下载的mui-master，找到里面的example使用
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vue-project-1/src/main.js
+++ vue-project-1/src/main.js
@@ -1,1 +1,21 @@
-console.log("ok")
+console.log("入口文件")
+
+
+import Vue from 'vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+import "./lib/mui/css/mui.min.css"
+
+import App from './App.vue'
+
+
+
+Vue.use(MintUI)
+
+var vm = new Vue({
+    el:"#app",
+    // 直接渲染app组件
+    render:c=>c(App)
+
+})
